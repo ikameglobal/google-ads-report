6132d46d2939e3f664bb611035a3aece82c31fbf
Minh <minhpc@ikameglobal.com>
2023-08-02 17:46:54 +0700

    add test case

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,16 +6,33 @@ import os
 
 import pytest
 
-from google_ads_report import SampleClass
+from google_ads_report import GoogleAdsApiClient
 
 
 @pytest.fixture
-def api_key():
-    api_key = os.environ.get("API_KEY", None)
-    assert api_key is not None
-    return api_key
+def cred_path():
+    cred_path = os.environ.get("CREDENTIALS_PATH", None)
+    assert cred_path is not None
+    return cred_path
 
 
-def test_report(api_key):
-    report = SampleClass(api_key=api_key)
-    assert report is not None
+def test_report(cred_path):
+    api_client = GoogleAdsApiClient(credentials_path=cred_path)
+    assert api_client is not None
+
+    query = f"""
+                SELECT
+                asset.id,
+                asset.name,
+                asset.type,
+                asset.text_asset.text,
+                asset.youtube_video_asset.youtube_video_title,
+                asset.youtube_video_asset.youtube_video_id,
+                asset.image_asset.full_size.url
+            FROM asset
+            """
+    result = []
+    for batch in api_client.get_response_batch(customer_id='2865142845', query=query):
+        result.extend(batch)
+
+    assert len(result) > 0
